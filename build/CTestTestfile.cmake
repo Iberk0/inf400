7a8e73485ec2a0cab57485c9768646638aa384a2
onuralpgunduz <onuralpgunduz@gmail.com>
2024-10-30 23:24:07 +0300

    Changed starter 30 ekim

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/onuralp/Desktop/ODEV1/hw1
+# Build directory: /home/onuralp/Desktop/ODEV1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/onuralp/Desktop/ODEV1/build/test_parser[1]_include.cmake")
+subdirs("_deps/googletest-build")
---
+++ hw1/kiraz/token/Literal.h
@@ -0,0 +1,50 @@
+#ifndef KIRAZ_TOKEN_LITERAL_H
+#define KIRAZ_TOKEN_LITERAL_H
+
+#include <kiraz/Token.h>
+
+namespace token {
+
+class Integer : public Token {
+public:
+    Integer(int64_t base, std::string_view value) 
+        : Token(L_INTEGER), m_base(base), m_value(value) {}
+    virtual ~Integer();
+
+    std::string as_string() const override {return fmt::format("Int{}", m_value);}
+
+    void print() {fmt::print("{}\n", as_string());}
+    static int colno;
+
+    auto get_base() const {return m_base;}
+    auto get_value() const {return m_value;}
+private:
+    int m_id;
+    int64_t m_base;
+    std::string m_value;
+};
+
+class Identifier : public Token {
+public:
+    Identifier(std::string_view name) : Token(IDENTIFIER), m_name(name) {}
+    virtual ~Identifier() {}
+
+    std::string as_string() const override { return fmt::format("Id({})", m_name); }
+
+    auto get_name() const { return m_name; }
+
+private:
+    std::string m_name;
+};
+
+class Import : public Token {
+public:
+    Import() : Token(KW_IMPORT) {}
+
+    std::string as_string() const override {return fmt::format("KW_IMPORT");}
+};
+
+
+}
+
+#endif // KIRAZ_TOKEN_LITERAL_H
